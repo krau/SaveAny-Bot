a25a58f8a2a1d5e479df7990b8bfa96697327589
krau <71133316+krau@users.noreply.github.com>
2025-02-01 17:01:46 +0800

    refactor(alist): replace req.Client with http.Client and improve error handling

--- go.mod
+++ go.mod
@@ -8,7 +8,6 @@ require (
 	github.com/gookit/slog v0.5.7
 	github.com/gotd/contrib v0.21.0
 	github.com/gotd/td v0.118.0
-	github.com/imroc/req/v3 v3.49.1
 	github.com/rhysd/go-github-selfupdate v1.2.3
 	github.com/spf13/cobra v1.8.1
 	github.com/spf13/viper v1.19.0
@@ -21,7 +20,6 @@ require (
 	github.com/AnimeKaizoku/cacher v1.0.2 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/cloudflare/circl v1.5.0 // indirect
 	github.com/coder/websocket v1.8.12 // indirect
 	github.com/dlclark/regexp2 v1.11.4 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
@@ -32,15 +30,12 @@ require (
 	github.com/go-faster/jx v1.1.0 // indirect
 	github.com/go-faster/xor v1.0.0 // indirect
 	github.com/go-faster/yaml v0.4.6 // indirect
-	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
 	github.com/google/go-github/v30 v30.1.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/pprof v0.0.0-20250128161936-077ca0a936bf // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gotd/ige v0.2.2 // indirect
 	github.com/gotd/neo v0.1.5 // indirect
-	github.com/hashicorp/errwrap v1.1.0 // indirect
-	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/inconshreveable/go-update v0.0.0-20160112193335-8152e7eb6ccf // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
@@ -48,11 +43,8 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/ogen-go/ogen v1.9.0 // indirect
-	github.com/onsi/ginkgo/v2 v2.22.2 // indirect
+	github.com/onsi/gomega v1.36.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/quic-go/qpack v0.5.1 // indirect
-	github.com/quic-go/quic-go v0.49.0 // indirect
-	github.com/refraction-networking/utls v1.6.7 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
 	github.com/tcnksm/go-gitconfig v0.1.2 // indirect
@@ -61,7 +53,6 @@ require (
 	go.opentelemetry.io/otel/metric v1.34.0 // indirect
 	go.opentelemetry.io/otel/trace v1.34.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
-	go.uber.org/mock v0.5.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.32.0 // indirect
 	golang.org/x/mod v0.22.0 // indirect
@@ -76,7 +67,6 @@ require (
 )
 
 require (
-	github.com/andybalholm/brotli v1.1.1 // indirect
 	github.com/coocood/freecache v1.2.4
 	github.com/duke-git/lancet/v2 v2.3.4
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
--- storage/alist/alist.go
+++ storage/alist/alist.go
@@ -1,6 +1,7 @@
 package alist
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
@@ -12,23 +13,23 @@ import (
 	"path"
 	"time"
 
-	"github.com/imroc/req/v3"
 	"github.com/krau/SaveAny-Bot/config"
 	"github.com/krau/SaveAny-Bot/logger"
 )
 
-type Alist struct{}
+type Alist struct {
+	client    *http.Client
+	token     string
+	basePath  string
+	baseURL   string
+	loginInfo *loginRequest
+}
 
 var (
-	basePath  string
-	baseUrl   string
-	reqClient *req.Client
-	loginReq  *loginRequset
-
 	ErrAlistLoginFailed = errors.New("failed to login to Alist")
 )
 
-type loginRequset struct {
+type loginRequest struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
@@ -39,52 +40,6 @@ type loginResponse struct {
 	Data    struct {
 		Token string `json:"token"`
 	} `json:"data"`
-}
-
-func getToken() (string, error) {
-	resp, err := reqClient.R().SetBodyJsonMarshal(loginReq).Post("/api/auth/login")
-	if err != nil {
-		return "", err
-	}
-	var loginResp loginResponse
-	if err := json.Unmarshal(resp.Bytes(), &loginResp); err != nil {
-		return "", err
-	}
-	if loginResp.Code != http.StatusOK {
-		return "", fmt.Errorf("%w: %s", ErrAlistLoginFailed, loginResp.Message)
-	}
-	return loginResp.Data.Token, nil
-}
-
-func refreshToken(client *req.Client) {
-	for {
-		time.Sleep(time.Duration(config.Cfg.Storage.Alist.TokenExp) * time.Second)
-		token, err := getToken()
-		if err != nil {
-			logger.L.Errorf("Failed to refresh jwt token: %v", err)
-			continue
-		}
-		client.SetCommonHeader("Authorization", token)
-		logger.L.Info("Refreshed Alist jwt token")
-	}
-}
-
-func (a *Alist) Init() {
-	basePath = config.Cfg.Storage.Alist.BasePath
-	baseUrl = config.Cfg.Storage.Alist.URL
-	reqClient = req.C().SetTLSHandshakeTimeout(time.Second * 10).SetBaseURL(baseUrl).SetTimeout(time.Hour * 24)
-	loginReq = &loginRequset{
-		Username: config.Cfg.Storage.Alist.Username,
-		Password: config.Cfg.Storage.Alist.Password,
-	}
-	token, err := getToken()
-	if err != nil {
-		logger.L.Fatalf("Failed to login to Alist: %v", err)
-		os.Exit(1)
-	}
-	logger.L.Debug("Logged in to Alist")
-	reqClient.SetCommonHeader("Authorization", token)
-	go refreshToken(reqClient)
 }
 
 type putResponse struct {
@@ -102,36 +57,122 @@ type putResponse struct {
 	} `json:"data"`
 }
 
+func (a *Alist) getToken() error {
+	loginBody, err := json.Marshal(a.loginInfo)
+	if err != nil {
+		return fmt.Errorf("failed to marshal login request: %w", err)
+	}
+
+	req, err := http.NewRequest(http.MethodPost, a.baseURL+"/api/auth/login", bytes.NewBuffer(loginBody))
+	if err != nil {
+		return fmt.Errorf("failed to create login request: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := a.client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to send login request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read login response: %w", err)
+	}
+
+	var loginResp loginResponse
+	if err := json.Unmarshal(body, &loginResp); err != nil {
+		return fmt.Errorf("failed to unmarshal login response: %w", err)
+	}
+
+	if loginResp.Code != http.StatusOK {
+		return fmt.Errorf("%w: %s", ErrAlistLoginFailed, loginResp.Message)
+	}
+
+	a.token = loginResp.Data.Token
+	return nil
+}
+
+func (a *Alist) refreshToken() {
+	for {
+		time.Sleep(time.Duration(config.Cfg.Storage.Alist.TokenExp) * time.Second)
+		if err := a.getToken(); err != nil {
+			logger.L.Errorf("Failed to refresh jwt token: %v", err)
+			continue
+		}
+		logger.L.Info("Refreshed Alist jwt token")
+	}
+}
+
+func (a *Alist) Init() {
+	a.basePath = config.Cfg.Storage.Alist.BasePath
+	a.baseURL = config.Cfg.Storage.Alist.URL
+	a.client = &http.Client{
+		Timeout: 12 * time.Hour,
+		Transport: &http.Transport{
+			TLSHandshakeTimeout: 10 * time.Second,
+		},
+	}
+	a.loginInfo = &loginRequest{
+		Username: config.Cfg.Storage.Alist.Username,
+		Password: config.Cfg.Storage.Alist.Password,
+	}
+
+	if err := a.getToken(); err != nil {
+		logger.L.Fatalf("Failed to login to Alist: %v", err)
+		os.Exit(1)
+	}
+	logger.L.Debug("Logged in to Alist")
+
+	go a.refreshToken()
+}
+
 func (a *Alist) Save(ctx context.Context, filePath, storagePath string) error {
-	storagePath = path.Join(basePath, storagePath)
+	storagePath = path.Join(a.basePath, storagePath)
 	file, err := os.Open(filePath)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to open file: %w", err)
 	}
 	defer file.Close()
-	fileBytes, err := io.ReadAll(file)
+
+	filestat, err := file.Stat()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to get file stats: %w", err)
 	}
-	resp, err := reqClient.R().
-		SetContext(ctx).
-		SetBodyBytes(fileBytes).
-		SetHeaders(map[string]string{
-			"File-Path": url.QueryEscape(storagePath),
-			"As-Task":   "true",
-		}).Put("/api/fs/put")
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPut, a.baseURL+"/api/fs/put", file)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create request: %w", err)
 	}
+	req.Header.Set("Authorization", a.token)
+	req.Header.Set("File-Path", url.PathEscape(storagePath))
+	req.Header.Set("As-Task", "true")
+	req.Header.Set("Content-Type", "application/octet-stream")
+	req.ContentLength = filestat.Size()
+
+	resp, err := a.client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to send request: %w", err)
+	}
+	defer resp.Body.Close()
+
 	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("failed to save file to Alist: %s", resp.Status)
 	}
-	var putResp putResponse
-	if err := json.Unmarshal(resp.Bytes(), &putResp); err != nil {
-		return fmt.Errorf("failed to unmarshal put response: %v", err)
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read response body: %w", err)
 	}
+
+	var putResp putResponse
+	if err := json.Unmarshal(body, &putResp); err != nil {
+		return fmt.Errorf("failed to unmarshal put response: %w", err)
+	}
+
 	if putResp.Code != http.StatusOK {
 		return fmt.Errorf("failed to save file to Alist: %d, %s", putResp.Code, putResp.Message)
 	}
+
 	return nil
 }
