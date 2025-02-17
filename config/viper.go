ccfde34666c1f1abadc46436b395e46769e07d33
krau <71133316+krau@users.noreply.github.com>
2025-02-17 16:37:46 +0800

    fix: failed to run when config file exist

--- config/viper.go
+++ config/viper.go
@@ -106,10 +106,7 @@ func Init() {
 	viper.SetDefault("storage.alist.base_path", "/")
 	viper.SetDefault("storage.alist.token_exp", 3600)
 
-	if err := viper.SafeWriteConfigAs("config.toml"); err != nil {
-		fmt.Println("Error writing default config file, ", err)
-		os.Exit(1)
-	}
+	viper.SafeWriteConfigAs("config.toml")
 
 	if err := viper.ReadInConfig(); err != nil {
 		fmt.Println("Error reading config file, ", err)
