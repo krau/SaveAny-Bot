a8edbb8bfbc83313a51086dc4de6cb5555ee7ae1
doomwithdon <doomwithdon@yahoo.com>
2025-02-19 10:33:59 -0500

    Update core.go

--- core/core.go
+++ core/core.go
@@ -1,71 +1,167 @@
-(chatId, []tg.InputMessageClass{&tg.InputMessageID{ID: messageID}})
+package core
+
+import (
+"context"
+"errors"
+"fmt"
+"io"
+"os"
+"path"
+"path/filepath"
+"time"
+
+"github.com/gabriel-vasile/mimetype"
+
+"github.com/celestix/gotgproto/ext"
+"github.com/duke-git/lancet/v2/fileutil"
+"github.com/gotd/td/tg"
+"github.com/krau/SaveAny-Bot/bot"
+"github.com/krau/SaveAny-Bot/config"
+"github.com/krau/SaveAny-Bot/logger"
+"github.com/krau/SaveAny-Bot/queue"
+"github.com/krau/SaveAny-Bot/storage"
+"github.com/krau/SaveAny-Bot/types"
+)
+
+func processPendingTask(task *types.Task) error {
+logger.L.Debugf("Start processing task: %s", task.String())
+if task.FileName() == "" {
+task.File.FileName = fmt.Sprintf("%d_%d_%s", task.FileChatID, task.FileMessageID, task.File.Hash())
+}
+cacheDestPath := filepath.Join(config.Cfg.Temp.BasePath, task.FileName())
+cacheDestPath, err := filepath.Abs(cacheDestPath)
 if err != nil {
-return nil, err
+return fmt.Errorf("Path processing failed: %w", err)
 }
-if len(messages) == 0 {
-return nil, errors.New("no messages found")
-}
-msg := messages[0]
-tgMessage, ok := msg.(*tg.Message)
-if !ok {
-return nil, fmt.Errorf("unexpected message type: %T", msg)
-}
-return tgMessage, nil
+if err := fileutil.CreateDir(filepath.Dir(cacheDestPath)); err != nil {
+return fmt.Errorf("Failed to create directory: %w", err)
 }
 
-func ProvideSelectMessage(ctx *ext.Context, update *ext.Update, file *types.File, chatID int64, fileMsgID, toEditMsgID int) error {
-entityBuilder := entity.Builder{}
-var entities []tg.MessageEntityClass
-text := fmt.Sprintf("File name: %s\nPlease select the storage location", file.FileName)
-if err := styling.Perform(&entityBuilder,
-styling.Plain("File name: "),
-styling.Code(file.FileName),
-styling.Plain("\nPlease select the storage location"),
-); err != nil {
-logger.L.Errorf("Failed to build entity: %s", err)
-} else {
-text, entities = entityBuilder.Complete()
+if task.StoragePath == "" {
+task.StoragePath = task.File.FileName
 }
-markup, err := getSelectStorageMarkup(update.EffectiveUser().GetID(), int(chatID), fileMsgID)
-if errors.Is(err, ErrNoStorages) {
-logger.L.Errorf("Failed to get select storage markup: %s", err)
-ctx.EditMessage(update.EffectiveChat().GetID(), &tg.MessagesEditMessageRequest{
-Message: "No storage available",
-ID: toEditMsgID,
-})
-return dispatcher.EndGroups
-} else if err != nil {
-logger.L.Errorf("Failed to get select storage markup: %s", err)
-ctx.EditMessage(update.EffectiveChat().GetID(), &tg.MessagesEditMessageRequest{
-Message: "Unable to obtain storage",
-ID: toEditMsgID,
-})
-return dispatcher.EndGroups
+
+taskStorage, err := storage.GetStorageByUserIDAndName(task.UserID, task.StorageName)
+if err != nil {
+return err
 }
-_, err = ctx.EditMessage(update.EffectiveChat().GetID(), &tg.MessagesEditMessageRequest{
+task.StoragePath = taskStorage.JoinStoragePath(*task)
+
+if task.File.FileSize == 0 {
+return processPhoto(task, taskStorage, cacheDestPath)
+}
+
+ctx := task.Ctx.(*ext.Context)
+
+barTotalCount := calculateBarTotalCount(task.File.FileSize)
+
+progressCallback := func(bytesRead, contentLength int64) {
+progress := float64(bytesRead) / float64(contentLength) * 100
+logger.L.Tracef("Downloading %s: %.2f%%", task.String(), progress)
+if task.File.FileSize < 1024*1024*50 || int(progress)%(100/barTotalCount) != 0 {
+return
+}
+text, entities := buildProgressMessageEntity(task, barTotalCount, bytesRead, task.StartTime, progress)
+ctx.EditMessage(task.ReplyChatID, &tg.MessagesEditMessageRequest{
 Message: text,
 Entities: entities,
-ReplyMarkup: markup,
-ID: toEditMsgID,
+ID: task.ReplyMessageID,
 })
-if err != nil {
-logger.L.Errorf("Failed to reply: %s", err)
-}
-return dispatcher.EndGroups
 }
 
-func HandleSilentAddTask(ctx *ext.Context, update *ext.Update, user *types.User, task *types.Task) error {
-if user.DefaultStorage == "" {
-ctx.EditMessage(update.EffectiveChat().GetID(), &tg.MessagesEditMessageRequest{
-Message: "Please use /storage to set the default storage location first",
+text, entities := buildProgressMessageEntity(task, barTotalCount, 0, task.StartTime, 0)
+ctx.EditMessage(task.ReplyChatID, &tg.MessagesEditMessageRequest{
+Message: text,
+Entities: entities,
 ID: task.ReplyMessageID,
 })
-return dispatcher.EndGroups
+
+readCloser, err := NewTelegramReader(task.Ctx, bot.Client, &task.File.Location,
+0, task.File.FileSize-1, task.File.FileSize,
+progressCallback, task.File.FileSize/100)
+if err != nil {
+return fmt.Errorf("Failed to create download: %w", err)
 }
-queue.AddTask(*task)
-ctx.EditMessage(update.EffectiveChat().GetID(), &tg.MessagesEditMessageRequest{
-Message: fmt.Sprintf("Added to queue: %s\nCurrent number of queued tasks: %d", task.FileName(), queue.Len()),
+defer readCloser.Close()
+
+dest, err := os.Create(cacheDestPath)
+if err != nil {
+return fmt.Errorf("Failed to create file: %w", err)
+}
+defer dest.Close()
+task.StartTime = time.Now()
+if _, err := io.CopyN(dest, readCloser, task.File.FileSize); err != nil {
+return fmt.Errorf("Failed to download file: %w", err)
+}
+defer cleanCacheFile(cacheDestPath)
+if path.Ext(task.FileName()) == "" {
+mimeType, err := mimetype.DetectFile(cacheDestPath)
+if err != nil {
+logger.L.Errorf("Failed to detect mime type: %s", err)
+} else {
+task.File.FileName = fmt.Sprintf("%s%s", task.FileName(), mimeType.Extension())
+task.StoragePath = fmt.Sprintf("%s%s", task.StoragePath, mimeType.Extension())
+}
+}
+
+logger.L.Infof("Downloaded file: %s", cacheDestPath)
+ctx.EditMessage(task.ReplyChatID, &tg.MessagesEditMessageRequest{
+Message: fmt.Sprintf("Download completed: %s\nTransferring file...", task.FileName()),
 ID: task.ReplyMessageID,
 })
-return dispatcher.EndGroups
+
+return saveFileWithRetry(task, taskStorage, cacheDestPath)
+}
+
+func worker(queue *queue.TaskQueue, semaphore chan struct{}) {
+for {
+semaphore <- struct{}{}
+task := queue.GetTask()
+logger.L.Debugf("Got task: %s", task.String())
+
+switch task.Status {
+case types.Pending:
+logger.L.Infof("Processing task: %s", task.String())
+if err := processPendingTask(&task); err != nil {
+logger.L.Errorf("Failed to do task: %s", err)
+task.Error = err
+if errors.Is(err, context.Canceled) {
+logger.L.Debugf("Task canceled: %s", task.String())
+task.Status = types.Canceled
+} else {
+task.Status = types.Failed
+}
+} else {
+task.Status = types.Succeeded
+}
+queue.AddTask(task)
+case types.Succeeded:
+logger.L.Infof("Task succeeded: %s", task.String())
+task.Ctx.(*ext.Context).EditMessage(task.ReplyChatID, &tg.MessagesEditMessageRequest{
+Message: fmt.Sprintf("File saved successfully\n [%s]: %s", task.StorageName, task.StoragePath),
+ID: task.ReplyMessageID,
+})
+case types. Failed:
+logger.L.Errorf("Task failed: %s", task.String())
+task.Ctx.(*ext.Context).EditMessage(task.ReplyChatID, &tg.MessagesEditMessageRequest{
+Message: "File save failed\n" + task.Error.Error(),
+ID: task.ReplyMessageID,
+})
+case types.Canceled:
+logger.L.Infof("Task canceled: %s", task.String())
+default:
+logger.L.Errorf("Unknown task status: %s", task.Status)
+}
+<-semaphore
+logger.L.Debugf("Task done: %s", task.String())
+}
+}
+
+func Run() {
+logger.L.Info("Start processing tasks...")
+semaphore := make(chan struct{}, config.Cfg.Workers)
+for i := 0; i < config.Cfg.Workers; i++ {
+go woLet guests (queue.Queue, semaphore)
+}
+
 }
