230c07fd55f6069f0a4906d68ec8bbd3e7720500
krau <71133316+krau@users.noreply.github.com>
2025-04-20 14:50:06 +0800

    feat: add rate limiting middleware to enhance bot performance

--- bot/middlewares.go
+++ bot/middlewares.go
@@ -1,18 +1,24 @@
 package bot
 
 import (
+	"time"
+
 	"github.com/celestix/gotgproto/dispatcher"
 	"github.com/celestix/gotgproto/ext"
 	"github.com/duke-git/lancet/v2/slice"
 	"github.com/gotd/contrib/middleware/floodwait"
+	"github.com/gotd/contrib/middleware/ratelimit"
 	"github.com/gotd/td/telegram"
 	"github.com/krau/SaveAny-Bot/config"
+	"golang.org/x/time/rate"
 )
 
 func FloodWaitMiddleware() []telegram.Middleware {
 	waiter := floodwait.NewSimpleWaiter().WithMaxRetries(5)
+	ratelimiter := ratelimit.New(rate.Every(time.Millisecond*100), 5)
 	return []telegram.Middleware{
 		waiter,
+		ratelimiter,
 	}
 }
 
--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/spf13/cobra v1.8.1
 	github.com/spf13/viper v1.19.0
 	golang.org/x/net v0.39.0
+	golang.org/x/time v0.8.0
 )
 
 require (
