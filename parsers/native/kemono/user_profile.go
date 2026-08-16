c7911cc208d84f7191d467afc8a407503394ea39
krau <71133316+krau@users.noreply.github.com>
2026-08-16 20:28:27 +0800

    refactor: remove commented-out dead code
    Drop unused kemono legacy types and commented response structs.

--- parsers/native/kemono/user_profile.go
+++
@@ -1,8 +0,0 @@
-package kemono
-
-type UserProfile struct {
-	ID       string  `json:"id"`
-	Name     string  `json:"name"`
-	Service  string  `json:"service"`
-	PublicID *string `json:"public_id,omitempty"`
-}
